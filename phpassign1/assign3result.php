9fcfec95eb33b7003ff00ceaecbac7a8477bbdd7
Souvik Banerjee <souvik.banerjee@innoraft.com>
2023-02-15 15:58:32 +0530

    FT2023-68: PHP refactoring

--- phpassign1/assign3result.php
+++ phpassign1/assign3result.php
@@ -6,9 +6,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
   <!-- style.css -->
   <link rel="stylesheet" href="css/style.css">
@@ -17,45 +14,45 @@
 </head>
 
 <body>
-  <div class="main bg-success">
-    <div class="main-container container">
+  <div class="main">
+    <div class="main-container container flex-col">
       <?php
-        require 'classFormData.php';
-        $formData = new FormData();
-        if (isset($_FILES['image']) && !isset($errors['fileType'])) {
-          $image = $_FILES['image'];
-          $formData->setImage($image);
-          $formData->uploadImage();
-        }
-      ?>
-      <img src='<?php echo "$formData->destination" ?>' alt="profile pic" height="300px">
-      <h4>
-        <?php
-          if (isset($_POST['submit'])) {
-            $formData->setFirstName($_POST['inputFirstName']);
-            $formData->setLastName($_POST['inputLastName']);
-            $fullName = $formData->inputFirstName . ' ' . $formData->inputLastName;
-            if ($formData->errorCheck()) {
-              // form data is valid
-              echo "Hello $fullName";
-            } else {
-              //   form data is not valid, show error messages
-              foreach ($errors as $value) {
-                echo "$value <br>";
-              }
-            }
+      require 'classFormData.php';
+      $formData = new FormData();
+      if (isset($_FILES['image']) && !isset($errors['fileType'])) {
+        $image = $_FILES['image'];
+        $formData->setImage($image);
+        $formData->uploadImage();
+        $formData->setFirstName($_POST['inputFirstName']);
+        $formData->setLastName($_POST['inputLastName']);
+        $fullName = $formData->inputFirstName . ' ' . $formData->inputLastName;
+        if (!$formData->errorCheck()) {
+          foreach ($formData->errors as $value) {
+            ?>
+            <h4>
+              <?php echo "$value"; ?>
+              <br>
+            </h4>
+          <?php
           }
-        ?>
-      </h4>
+          die();
+        }
+      }
+      ?>
+      <img src='<?php echo "$formData->destination" ?>' alt="profile pic" class="profile-pic">
+      <h2>
+        Hello
+        <?php echo "$fullName"; ?>
+      </h2>
       <table>
         <tr>
           <th>Subject</th>
           <th>Marks</th>
         </tr>
-        <?php 
-          if (isset($_POST['submit'])) {
-            $formData->tableData($_POST['marks']);
-          }
+        <?php
+        if (isset($_POST['submit'])) {
+          $formData->tableData($_POST['marks']);
+        }
         ?>
       </table>
     </div>
