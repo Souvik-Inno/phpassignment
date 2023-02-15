99cbe42274e82c6505202e4b2caa45fe9ce80b56
Souvik Banerjee <souvik.banerjee@innoraft.com>
2023-02-15 15:56:03 +0530

    FT2023-68: PHP refactoring

--- phpassign1/assign5result.php
+++ phpassign1/assign5result.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,84 +10,67 @@
   <!-- style.css -->
   <link rel="stylesheet" href="css/style.css">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
   <title>Assign1 Result</title>
+  <!-- jquery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 
 <body>
   <div class="main bg-success">
-    <div class="main-container container">
+    <div class="main-container container flex-col">
       <?php
         require 'classFormData.php';
         $formData = new FormData();
         if (isset($_FILES['image']) && !isset($errors['fileType'])) {
           $image = $_FILES['image'];
           $formData->setImage($image);
+          $formData->setFirstName($_POST['inputFirstName']);
+          $formData->setLastName($_POST['inputLastName']);
+          $fullName = $formData->inputFirstName . ' ' . $formData->inputLastName;
+          $formData->setPhone($_POST['inputPhone']);
+          $formData->setEmailId($_POST['inputEmail']);
+
+          if (!$formData->fullErrorCheck()) {
+            foreach ($formData->errors as $value) {
+              ?>
+              <h4>
+                <?php echo "$value"; ?>
+                <br>
+              </h4>
+              <?php
+            }
+            die();
+          }
           $formData->uploadImage();
         }
       ?>
-      <img src='<?php echo "$formData->destination" ?>' alt="profile pic" height="300px">
-      <h4>
-        <?php
-          if (isset($_POST['submit'])) {
-            $formData->setFirstName($_POST['inputFirstName']);
-            $formData->setLastName($_POST['inputLastName']);
-            $fullName = $formData->inputFirstName . ' ' . $formData->inputLastName;
-            if ($formData->errorCheck()) {
-              // form data is valid
-              echo "Hello $fullName";
-            } else {
-              //   form data is not valid, show error messages
-              foreach ($errors as $value) {
-                echo "$value <br>";
-              }
-            }
-          }
-        ?>
-      </h4>
+      <img src='<?php echo "$formData->destination" ?>' alt="profile pic" class="profile-pic">
+      <h2>
+        Hello
+        <?php echo "$fullName" ?>
+      </h2>
       <table>
         <tr>
           <th>Subject</th>
           <th>Marks</th>
         </tr>
-        <?php 
+        <?php
           if (isset($_POST['submit'])) {
             $formData->tableData($_POST['marks']);
           }
         ?>
       </table>
-          <?php 
-            if (isset($_POST['submit'])) {
-              $formData->setPhone($_POST['inputPhone']);
-            }
-          ?>
       <h4>
-          Your Phone number is: <?php echo "$formData->phoneNumber" ?>
+        Your Phone number is:
+        <?php echo "$formData->phoneNumber" ?>
       </h4>
-      <?php 
+      <?php
         if (isset($_POST['submit'])) {
-          $formData->setEmailId($_POST['inputEmail']);
           $formData->checkEmail();
         }
       ?>
     </div>
   </div>
-
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"></script>
 </body>
 
 </html>
